7344004701b4b8e540e4a94eb4c8bef6a62c03b5
Aymanelahmidi <136036903+Aymanelahmidi@users.noreply.github.com>
2024-07-19 21:32:16 -0400

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,9 +0,0 @@
-# 0-strace_is_your_friend.pp
-exec { 'fix-apache':
-  command => '/bin/chown -R www-data:www-data /var/www/html',
-  onlyif  => '/usr/bin/test $(/usr/bin/stat -c %U /var/www/html) != "www-data"',
-}
-
-
-
-
